a876b6249873ece85dfdf21724aec01110e77190
Atzelcer <atzelcer@gmail.com>
2024-06-12 17:45:49 -0400

    --dsf

--- MyForm.cpp
+++ MyForm.cpp
@@ -8,9 +8,6 @@ void Main(array<String^>^ args) {
     Application::EnableVisualStyles();
     Application::SetCompatibleTextRenderingDefault(false);
 
-    // Inicializa tu aplicación aquí, si es necesario.
-    // Por ejemplo, carga datos iniciales, configura conexiones, etc.
-
     ArbolesGeneralesPasteleria::MyForm form; // Crea una instancia de tu formulario principal.
     Application::Run(% form);
 }
